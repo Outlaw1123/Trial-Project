9d0a98ed405c4aaba36adecedfd40a2e03132990
Christian Mertins <mertins.christian@gmx.de>
2014-10-22 13:29:33 +0200

    first branch

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+	private PlayerController playerController;
+
+	private Transform defuse; // drag the player here
+	public float speed = 5.0f; // move speed
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script!");
+		}
+
+		GameObject playerControllerObject = GameObject.FindWithTag ("Player");
+		if (playerControllerObject != null)
+		{
+			playerController = playerControllerObject.GetComponent <PlayerController>();
+		}
+		if (playerController == null)
+		{
+			Debug.Log ("Cannot find 'PlayerController' script!");
+		}
+	}
+	
+	void Update()
+	{
+		GameObject bombDefuseBoundary = GameObject.FindWithTag ("BombDefuseBoundary");
+		defuse = bombDefuseBoundary.GetComponent<Transform> ();
+		transform.position = Vector3.MoveTowards(transform.position, defuse.position, -speed*Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		Debug.Log ("Collision: " + collider.name + "\tWith: " + other.gameObject.name);
+
+		if (other.tag == "SpawnBoundary" || other.tag == "BombDefuseBoundary")
+		{
+			return;
+		}
+
+		else if (other.tag == "Player")
+		{
+			int life = playerController.DecreaseLife ();
+			if (life == 0)
+			{
+				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+				Destroy (other.gameObject);
+				gameController.GameOver ();
+			}
+		}
+
+		else
+		{
+			gameController.AddScore (scoreValue);
+			Destroy (other.gameObject);
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "BombDefuseBoundary")
+		{
+			Destroy (gameObject);
+			return;
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	public int life;
+	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	
+	public Quaternion rotation;
+	public Vector3 radius = new Vector3(0,0,0);
+	public float currentRotation = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		//rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+		currentRotation += Input.GetAxis ("Horizontal")*Time.deltaTime*-100;
+		rotation.eulerAngles = new Vector3(0, currentRotation, -9);
+		transform.position = rotation * radius;
+		transform.rotation = rotation;
+		
+	}
+}
